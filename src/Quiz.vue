8643a8986cf88dfa7c9229317372b31f96693c6c
Kai McKeever <kaimckeever@gatech.edu>
2021-10-12 17:34:25 -0400

    create begin quiz button and first question upon button click

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,17 +1,29 @@
 <template>
-  <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+  <div>
+    <div v-if="!this.started">
+      <button v-on:click="started = true">Begin Quiz</button>
+    </div>
+    <div v-if="this.started">
+      <Question :question="questions[i]"></Question>
+      <button>Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Question from "./components/Question.vue";
 export default {
   props: {
     questions: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
+  },
+  components: {
+    Question,
   },
   data() {
-    return {};
-  }
+    return { started: false, i: 0 };
+  },
 };
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ question.text }}</h1>
+    <form>
+      <div v-for="ans in question.answers" :key="ans">
+        <input type="radio" name="ans" :value="ans" v-model="answer" />
+        <label>{{ ans }}</label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
