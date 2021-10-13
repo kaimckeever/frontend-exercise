a30f4a95c45c030e0b19a588bd435d02ddfd3cbc
Kai McKeever <kaimckeever@gatech.edu>
2021-10-13 11:51:03 -0400

    remove unneeded imports (continued)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
-import { MdButton, MdContent, MdRadio, MdLayout, } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/black-green-light.css'
 import Quiz from "./Quiz.vue";
 import questions from "./questions";
 import Buefy from 'buefy'
