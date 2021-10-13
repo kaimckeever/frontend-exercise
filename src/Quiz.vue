11b84129bb1ea28d9b0de0403962f99840cc7215
Kai McKeever <kaimckeever@gatech.edu>
2021-10-13 10:28:54 -0400

    add Material Design layout and components

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,23 +1,50 @@
 <template>
-  <div>
-    <div v-if="!this.started">
-      <button v-on:click="started = true">Begin Quiz</button>
+  <div class="md-layout md-alignment-center">
+    <div
+      class="
+        md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100
+      "
+      v-if="!this.started"
+    >
+      <md-content>
+        <h1>Cognito Forms Quiz</h1>
+        <p>
+          You will be presented with a series of multiple choice questions, and
+          when you've answered all of the questions, you can see a summary of
+          your answers.
+        </p>
+        <button class="md-button" v-on:click="started = true">
+          Begin Quiz
+        </button>
+      </md-content>
     </div>
-    <div v-if="this.started && i < questions.length">
+    <div
+      class="
+        md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100
+      "
+      v-if="this.started && i < questions.length"
+    >
       <h1>{{ questions[i].text }}</h1>
       <form>
         <div v-for="ans in questions[i].answers" :key="ans">
-          <input type="radio" name="ans" v-model="answer" :value="ans" />
-          <label>{{ ans }}</label>
+          <md-radio type="radio" name="ans" v-model="answer" :value="ans">
+            {{ ans }}
+          </md-radio>
         </div>
       </form>
-      <button v-show="answer != ''" v-on:click="next()">Next</button>
+      <button class="md-button" v-show="answer != ''" v-on:click="next()">
+        <div v-if="i == questions.length - 1">Submit</div>
+        <div v-else>Next</div>
+      </button>
     </div>
     <div v-if="this.started && i >= questions.length">
-      <div v-for="(q, index) in questions" :key="q">
-        <b>{{ q.text }}</b>
-        <p>{{ answerList[index] }}</p>
-      </div>
+      <md-content>
+        <h1>Summary</h1>
+        <div v-for="(q, index) in questions" :key="q">
+          <h2>{{ q.text }}</h2>
+          <p>{{ answerList[index] }}</p>
+        </div>
+      </md-content>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from "vue";
+import Vue from 'vue'
+import { MdButton, MdContent, MdRadio, MdLayout, } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/black-green-light.css'
 import Quiz from "./Quiz.vue";
 import questions from "./questions";
 
 Vue.config.productionTip = false;
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdRadio)
+Vue.use(MdLayout)
 
 new Vue({
   render: h => h(Quiz, { props: { questions } })
